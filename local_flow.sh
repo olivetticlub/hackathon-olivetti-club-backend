a75e6ad8de5299f4f7b039b30a49250b450d1c6b
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2019-10-05 14:33:55 +0200

    replace example with a serious one

--- local_flow.sh
+++ local_flow.sh
@@ -4,7 +4,7 @@ echo "+++ Creating merchant"
 curl -X POST -H 'Content-Type: application/json' -i http://localhost:5000/merchants --data '{"name": "danielefongo", "vat_number": "12345", "ateco": "01.11.10", "address": "Somewhere in Umbria"}'
 
 echo "+++ Creating coupons"
-curl -X POST -H 'Content-Type: application/json' -i http://localhost:5000/coupons --data '{"merchant": "danielefongo", "description": "cazzettini al 50%","count":2}'
+curl -X POST -H 'Content-Type: application/json' -i http://localhost:5000/coupons --data '{"merchant": "danielefongo", "description": "Buy one beer, get another for free","count":2}'
 
 echo "+++ Retrieving merchant info"
 curl http://localhost:5000/merchants/danielefongo
